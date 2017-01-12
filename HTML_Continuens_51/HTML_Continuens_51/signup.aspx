db70769afe71f10d12998c111efefad2a582c586
OdedHirsh <hirshoded@walla.com>
2017-01-12 20:55:16 +0200

    HTML Lesson 51-54

---
+++ HTML_Continuens_51/HTML_Continuens_51/signup.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="signup.aspx.cs" Inherits="HTML_Continuens_51.signup" %>
+
---
+++ HTML_Continuens_51/HTML_Continuens_51/signup.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.IO;
+
+namespace HTML_Continuens_51
+{
+    public partial class signup : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (StreamReader StreamReader = new StreamReader(Request.InputStream))
+            {
+                int oneByte;
+                while ((oneByte = StreamReader.Read()) != -1)
+                {
+                    char c = (char)oneByte;
+                    Debug.Print(c.ToString());
+                }
+
+
+            }
+            
+        }
+    }
+}
