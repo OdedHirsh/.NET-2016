d9c216f8b73c14963de9ddddb5ef878749149341
OdedHirsh <hirshoded@walla.com>
2017-04-24 21:32:44 +0300

    WPF-TicTacToe

---
+++ WpfCanvas/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TicTacToe game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new TicTacToe();
+        }
+
+
+
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            int cell = int.Parse(btn.Name.Substring(3));
+            MoveResult moveResult = game.makeMove(cell);
+            if (moveResult==MoveResult.INVALID_MOVE)
+            {
+                MessageBox.Show("What are you doing");
+                return;
+            }
+            btn.Content = game.isXTurn ? "O" : "X";
+            if (moveResult != MoveResult.INVALID_MOVE)
+            {
+                if (moveResult == MoveResult.VICTORY)
+                {
+                    MessageBox.Show((game.isXTurn?"O":"X")+" is the Winner","we have a Winner");
+                    return;
+                }
+                else if (moveResult==MoveResult.DRAW)
+                {
+                    MessageBox.Show("DRAW!!");
+                    return;
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            game.reset();
+            foreach (UIElement element in gridBoard.Children)
+            {
+                ((Button)element).Content = "";
+            }
+        }
+    }
+}
---
+++ WpfCanvas/TicTacToe.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCanvas
+{
+    public enum CellValue
+    {
+        X, O, EMPTY
+    }
+
+    public enum MoveResult
+    {
+        VALID_MOVE, INVALID_MOVE, VICTORY, DRAW
+    }
+
+    class TicTacToe
+    {
+        private CellValue[] board;
+        public bool isXTurn;
+        private int moveCount;
+
+        public TicTacToe()
+        {
+            board = new CellValue[9];
+            reset();
+        }
+
+        public void reset()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = CellValue.EMPTY;
+            }
+            isXTurn = true;
+            moveCount = 0;
+        }
+
+        public MoveResult makeMove(int cell)
+        {
+            if (cell < 1 || cell > 9)
+                throw new Exception("cell must be 1-9");
+            cell--;
+            if (board[cell] == CellValue.EMPTY)
+            {
+                moveCount++;
+                board[cell] = isXTurn ? CellValue.X : CellValue.O;
+                isXTurn = !isXTurn;
+                if (moveCount >= 5 && checkVictory(cell))
+                {
+                    return MoveResult.VICTORY;
+                }
+                if (moveCount == 9)
+                    return MoveResult.DRAW;
+                return MoveResult.VALID_MOVE;
+            }
+            return MoveResult.INVALID_MOVE;
+        }
+
+        private bool checkVictory(int cell)
+        {
+            int row = cell / 3;
+            int column = cell % 3;
+            if (board[column] == board[column + 3] && board[column] == board[column + 6])
+                return true;
+            row *= 3;
+            if (board[row] == board[row + 1] && board[row] == board[row + 2])
+                return true;
+            if (cell%2 ==0)
+            {
+                bool diagonal1 = board[0] != CellValue.EMPTY && board[0] == board[4] && board[0] == board[8];
+                if (diagonal1)
+                {
+                    return true;
+                }
+                bool diagonal2 = board[2] != CellValue.EMPTY && board[2] == board[4] && board[2] == board[6];
+                if (diagonal2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
